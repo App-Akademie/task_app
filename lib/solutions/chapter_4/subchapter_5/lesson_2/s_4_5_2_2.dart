4034a70971b9e6245c479bd191fc44af76a2eae1
Your Name <you@example.com>
2024-06-25 20:03:59 +0200

    Solved 4522

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,32 @@ class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Hello WOP !!!',
+          style: TextStyle(fontSize: 24),
+        ),
+        SizedBox(
+            height: 20), // Adds some spacing between the text and the button
+        ElevatedButton(
+          onPressed: () {
+            // Define the action when the button is pressed
+            // print('Habbidscsh gedrückt');
+          },
+          child: Text('Klick mich'),
+        ),
+      ],
+    );
   }
 }
