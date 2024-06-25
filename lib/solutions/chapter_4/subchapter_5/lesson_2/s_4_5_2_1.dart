668729ee7e9147f9c27fba9e24760d4548b9e406
Ralf Podchull <app@projectdelight.de>
2024-06-25 15:29:55 +0200

    4521 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,34 @@ class S4521 extends StatelessWidget {
   const S4521({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const HelloWorld();
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: const S4521(),
+      ),
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Hello World'),
+    );
   }
 }
