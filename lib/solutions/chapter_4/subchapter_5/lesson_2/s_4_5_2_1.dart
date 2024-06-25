d2186d42f2c9517621c6e2af4c8d547815a4ff17
Your Name <you@example.com>
2024-06-25 19:32:29 +0200

    solved 4521

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,23 @@ class S4521 extends StatelessWidget {
   const S4521({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const HelloWorldCenter();
+  }
+}
+
+class HelloWorldCenter extends StatelessWidget {
+  const HelloWorldCenter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Hello World',
+        style: TextStyle(
+        fontSize: 30,
+        color: Color.fromARGB(255, 255, 2, 2),
+        fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
