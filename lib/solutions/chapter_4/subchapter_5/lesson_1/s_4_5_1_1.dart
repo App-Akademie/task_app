8ad5be3d6c33b61f4f7fa85d2a469ef74981e855
Mikael2392 <mikaelsmiri@gmail.com>
2024-06-25 13:24:50 +0200

    task4511

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -4,6 +4,15 @@ class S4511 extends StatelessWidget {
   const S4511({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const HelloWorld();
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World");
   }
 }
