b6978d8468d934d7402be46247481151c54bc8c1
Rafael <rafael.a.ferreirasantos93@gmail.com>
2024-06-25 14:35:16 +0200

    World

--- lib/tasks/task_list.dart
+++ lib/tasks/task_list.dart
@@ -40,7 +40,7 @@ const List<Task> chapter3TaskList = [
 const List<Task> chapter4TaskList = [
   Task(
     taskModel: t_4_5_1_1,
-    //solution: S4511(),
+    solution: S4511(),
   ),
   Task(
     taskModel: t_4_5_2_1,
