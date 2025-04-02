5689166738ab23486a8bcbfc72b9eb27fafc7371
David Sent <david.sent@app-akademie.com>
2025-04-02 06:49:18 +0200

    remove solution

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,23 +4,6 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    return MyWidget();
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Test'),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text('Button'),
-        ),
-      ],
-    );
+    throw UnimplementedError();
   }
 }
